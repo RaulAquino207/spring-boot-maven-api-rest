ba9250b10a3c8548b0949701d2e2e4ea04a8422b
Raul Aquino <aquinoraul207@gmail.com>
2022-05-25 13:15:18 -0300

    added initial spring boot project

---
+++ virtualstore/src/main/java/br/com/studies/virtualstore/VirtualstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.studies.virtualstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualstoreApplication.class, args);
+	}
+
+}
