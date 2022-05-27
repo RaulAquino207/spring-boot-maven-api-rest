eccccc87dde112788a831a0bc74a37aee0ea6823
Raul Aquino <aquinoraul207@gmail.com>
2022-05-27 15:42:23 -0300

    added swagger to application

--- apirest/src/main/java/com/studies/apirest/controllers/PersonController.java
+++ apirest/src/main/java/com/studies/apirest/controllers/PersonController.java
@@ -3,6 +3,7 @@ package com.studies.apirest.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,40 +16,49 @@ import org.springframework.web.bind.annotation.RestController;
 import com.studies.apirest.models.Person;
 import com.studies.apirest.services.PersonService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
-@RequestMapping(value = "/api/person")
+@RequestMapping(value = "/api")
+@Api(value = "Person")
+@CrossOrigin(origins = "*")
 public class PersonController {
 	
 	@Autowired
 	private PersonService personService;
 	
-	@GetMapping(value = "/findAll")
+	@GetMapping(value = "/person/findAll")
+	@ApiOperation(value = "List of all persons in DB")
 	public ResponseEntity<Object> findAll(){
 		return personService.findAll();
 	}
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/person/{id}")
+//	@ApiOperation(value = "Find person by their ID")
 	public Object findById(
 			@PathVariable(value = "id") Long id){
 		return personService.findById(id);
 	}
 	
-	@PostMapping(value = "/create")
+	@PostMapping(value = "/person/create")
+//	@ApiOperation(value = "Create person")
 	public Person create(@RequestBody Person person) {
 		return personService.create(person);
 	}
 	
-	@DeleteMapping(value = "/{id}")
+	@DeleteMapping(value = "/person/{id}")
+//	@ApiOperation(value = "Delete person")
 	public ResponseEntity<Object> delete(
 			@PathVariable(value = "id") Long id) {
 		return personService.delete(id);
 	}
 	
-	@PutMapping(value = "/update")
+	@PutMapping(value = "/person/update")
+//	@ApiOperation(value = "Update person")
 	public ResponseEntity<Object> update(
 			@RequestBody Person person) {
 		return personService.update(person);
 	}
-	
 	
 }
--- apirest/src/main/java/com/studies/apirest/controllers/RoleController.java
+++ apirest/src/main/java/com/studies/apirest/controllers/RoleController.java
@@ -3,6 +3,7 @@ package com.studies.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,25 +14,29 @@ import org.springframework.web.bind.annotation.RestController;
 import com.studies.apirest.models.Role;
 import com.studies.apirest.services.RoleService;
 
+import io.swagger.annotations.Api;
+
 @RestController
-@RequestMapping(value = "/api/role")
+@RequestMapping(value = "/api")
+@Api(value = "Role")
+@CrossOrigin(origins = "*")
 public class RoleController {
 	
 	@Autowired
 	private RoleService roleService;
 	
-	@GetMapping(value = "/findAll")
+	@GetMapping(value = "/role/findAll")
 	public List<Role> findAll(){
 		return roleService.findAll();
 	}
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/role/{id}")
 	public Object findById(
 			@PathVariable(value = "id") Long id){
 		return roleService.findById(id);
 	}
 	
-	@PostMapping(value = "/create")
+	@PostMapping(value = "/role/create")
 	public Role create(@RequestBody Role role) {
 		return roleService.create(role);
 	}
